1b71b4ba6403ca2153f8e87aa837c048efa97b2c
Nathan <nathan.smith1990@hotmail.com>
2020-05-08 16:47:03 -0500

    Added blog post about World of Health

---
+++ BlogPosts/WorldOfHealth/InitialDeployment/scripts/chat-data.js
@@ -0,0 +1,10 @@
+const message = "entry.1624809253";
+const name = "entry.2099706026";
+const formID = "1FAIpQLSfAvJlI9lsIwZcsNV3RcZmF5IDvQHk5lvlU5V-9GiDzqYTqbA";
+
+const urlPOST = `https://docs.google.com/forms/d/e/1FAIpQLSfAvJlI9lsIwZcsNV3RcZmF5IDvQHk5lvlU5V-9GiDzqYTqbA/formResponse`;
+
+let sheetID = '1iSKeOiEp5id3T5nGUfLeC-vgYMJr_ojysIG6HZPEYUE';
+const key = 'AIzaSyABIAa7OMBgiOifb-ij84tIVeyrFlyeUXc';
+const sheet = encodeURIComponent('Form Responses 1');
+const urlGET = 'https://sheets.googleapis.com/v4/spreadsheets/1iSKeOiEp5id3T5nGUfLeC-vgYMJr_ojysIG6HZPEYUE/values/Form%20Responses%201?key=AIzaSyABIAa7OMBgiOifb-ij84tIVeyrFlyeUXc';
