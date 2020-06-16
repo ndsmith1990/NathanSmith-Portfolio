6d2ca2d8eaf57f4b6461ddb018a1d77318fb471b
Nathan <nathan.smith1990@hotmail.com>
2020-06-16 16:27:20 -0500

    deleted some stuff

--- BlogPosts/commonScripts/chat-view.js
+++ BlogPosts/commonScripts/chat-view.js
@@ -12,7 +12,6 @@ const showComments = function(sheetData){
 const addComment = function(row){
   const [time, name, message] = row;
   const chatList = document.getElementById('chat-list');
-  let id = 0;
   chatList.innerHTML += `<li class="bg-white"><div class="font-italic">${time}: ${name}</div> <div>${message}</div><hr class="comment-hr"></li>`;
 }
 
